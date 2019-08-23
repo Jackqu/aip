76abe3db6261a933b795dd77fc110856f8a63f31
qxc <qxc@meitu.com>
2019-08-23 09:30:14 +0800

    Add body kpt example

---
+++ scripts/body_kpt.py
@@ -0,0 +1,11 @@
+from aip import AipBodyAnalysis
+APP_ID = '17067353'
+API_KEY = 'ovdLIpCXNFAHHYXCASwVrO4u'
+SECRET_KEY = 'eyCudNEFKkQtH9KPfvyLr16KMendt1ir'
+client = AipBodyAnalysis(APP_ID, API_KEY, SECRET_KEY)
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+image = get_file_content('./test_imgs/body_1.jpg')
+print(client.bodyAnalysis(image))
